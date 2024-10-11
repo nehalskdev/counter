6108a1f1d1811b1eaeac9b698563809f073dfb9d
nehalskdev <nikesk1999@gmail.com>
2024-10-12 01:49:26 +0530

    targeted elements in js

---
+++ app.js
@@ -0,0 +1,10 @@
+"strict mode"
+
+const add = document.getElementById("Add");
+const minus = document.getElementById("Minus");
+const reset = document.getElementById("reset");
+
+add.addEventListener('click', () => {
+    console.log("add")
+});
+
