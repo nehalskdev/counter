45349e8f94a1eb7e32e555126f01361a9e301a47
nehalskdev <nikesk1999@gmail.com>
2024-10-12 02:02:27 +0530

    implimented logic for increament decreament and reset

--- app.js
+++ app.js
@@ -3,8 +3,29 @@
 const add = document.getElementById("Add");
 const minus = document.getElementById("Minus");
 const reset = document.getElementById("reset");
+const display = document.querySelector(".display-num");
+
+// initializing conter 
+let counter = 0;
+display.value = counter;
 
 add.addEventListener('click', () => {
-    console.log("add")
+    console.log("add");
+    counter++;
+    display.value = counter;
+
 });
+
+minus.addEventListener('click', () => {
+    console.log('Minus');
+    counter--;
+    display.value = counter;
+});
+
+reset.addEventListener("click", () => {
+    console.log('reset')
+    counter = 0;
+    display.value = counter;
+});
+
 
